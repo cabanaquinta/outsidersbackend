9d206395d4307f0657281fe4e00eabc8b5a7f7da
pereza <alejandro.perez@socialbakers.com>
2022-10-07 01:09:19 +0200

    Changes to my-project noted

--- config/plugins.js
+++ config/plugins.js
@@ -14,5 +14,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
+
   // ...
 });
